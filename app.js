122aab1c52eb7240c838a5027436b884fef09a83
Albert Yin <albyin@gmail.com>
2015-03-11 16:09:49 -0400

    set up html templating with swig

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 var morgan = require('morgan');
 var express = require( 'express' );
 var app = express();
+var swig = require('swig');
 
 app.use(morgan('dev'));
 
 app.listen("3000");
 
+app.engine("html", swig.renderFile);
+app.set("view engine", "html");
+app.set("views", __dirname+"/views");
+
+swig.setDefaults({ cache: false }); //turn off caching
+
+
+var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
 app.get("/", function(req, res){
 
-	res.send("Hello, world!");
+	res.render( 'index', {title: 'Hall of Fame', people: people} );
 });
 
 app.get("/news", function(req, res){
