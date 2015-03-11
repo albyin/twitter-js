d0bb391b9ee6130d0fe397e2630d6ce447c91cbe
Albert Yin <albyin@gmail.com>
2015-03-11 16:33:30 -0400

    added subrouter and dynamic routing

--- app.js
+++ app.js
@@ -2,31 +2,18 @@ var morgan = require('morgan');
 var express = require( 'express' );
 var app = express();
 var swig = require('swig');
+swig.setDefaults({ cache: false }); //turn off caching
+
+var routes = require('./routes');
 
 app.use(morgan('dev'));
-
 app.listen("3000");
-
 app.engine("html", swig.renderFile);
 app.set("view engine", "html");
 app.set("views", __dirname+"/views");
 
-swig.setDefaults({ cache: false }); //turn off caching
+app.use('/', routes);
+app.use(express.static(__dirname + '/public'));
 
 
-var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 
-app.get("/", function(req, res){
-
-	res.render( 'index', {title: 'Hall of Fame', people: people} );
-});
-
-app.get("/news", function(req, res){
-
-	res.send("Hello from news! david addition");
-});
-
-app.get("/updated", function(req, res){
-
-	res.send("Hello from updated!");
-});
